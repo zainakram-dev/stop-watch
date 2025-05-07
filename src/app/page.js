0520ada3aa9475cbbf0c8a5ba0a0fcf377f4db05
zainakram-dev <zain341802@gmail.com>
2025-05-07 23:56:23 +0500

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,86 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+"use client"
+import {useState,useRef} from "react"
+
+export default function StopWatch(){
+  const[time,setTime] = useState(0);
+  const intervalRef=useRef(null)
+
+const startTimer=()=>{
+ if(intervalRef.current!==null) return;
+ intervalRef.current=setInterval(()=>{setTime((prevTime)=>prevTime+10)},10)
+};
+const stopTimer=()=>{clearInterval(intervalRef.current);
+intervalRef.current=null}
+const resetTimer=()=>{
+  stopTimer();
+  setTime(0)
 }
+
+const formatTime=(time)=>{ 
+const minutes=Math.floor(time / 60000);
+const seconds=Math.floor((time % 60000)/1000);
+const milliseconds=Math.floor((time % 1000)/10)
+return (
+  `${String(minutes).padStart(2,"0")}:`+
+  `${String(seconds).padStart(2,"0")}.`+
+  `${String(milliseconds).padStart(2,"0")}`
+)
+}; 
+
+  return(
+    <div style={styles.contener}><span style={styles.watch}>{formatTime(time)}</span>
+    <button style={styles.startBtn} onClick={startTimer}>Start</button>
+    <button style={styles.stopBtn} onClick={stopTimer}>Stop</button>
+    <button style={styles.resetBtn} onClick={resetTimer}>ReSet</button>
+    </div>
+  )
+
+}
+const styles={
+contener:{
+  margin:"300px 0px 0px 400px",
+
+  
+},
+  watch:{
+    margin:"20px",
+    padding:'20px',
+fontSize:"50px",
+    textAlign:"center",
+color:"white",
+backgroundColor:"blue",  
+border:"2px solid",
+borderColor:"green",
+borderRadius:"15px"
+},
+startBtn:{
+  padding:"5px",
+  margin:"5px",
+  border:"2px solid",
+  borderColor:"white",
+  borderRadius:"20px",
+  backgroundColor:"green",
+},
+stopBtn:{
+  padding:"5px",
+  margin:"5px",
+  border:"2px solid",
+  borderColor:"white",
+  borderRadius:"20px",
+  backgroundColor:"green",
+},
+resetBtn:{
+  padding:"5px",
+  margin:"5px",
+  border:"2px solid",
+  borderColor:"white",
+  borderRadius:"20px",
+  backgroundColor:"green",
+},
+
+}
+
+
